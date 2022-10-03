b79c4d8b0b9c7ac264b16ebde81dc9093be8043e
Lukas Pellny <82966413+exceptionD@users.noreply.github.com>
2022-10-03 20:08:28 +0200

    Update jump.java

--- src/main/java/net/pellnyit/networkcore/commands/jump.java
+++ src/main/java/net/pellnyit/networkcore/commands/jump.java
@@ -1,1 +1,15 @@
 
+package net.pellnyit.networkcore.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class jump implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.hasPermission("perm.command.jump");
+        sender.sendMessage("you cant jump here.");
+        return false;
+    }
+}
